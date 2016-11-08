c873d655563d51a4be273432b937c62beb9fe21d
Desarrollo <Desarrollo@172.21.24.105>
2016-11-08 16:35:37 -0500

    cambios en pantalla de procesos + grilla de tareas,+ actualizaciones en usuarios BPM+ roles multiple seleccion.

---
+++ web/procesos/js/popUpGrilla.js
@@ -0,0 +1,10 @@
+/**
+ * funcion para crear elementos del pop up, se crea el primer for para crear
+ * todos los elementos del servicio, segundo for deja habilidatos los roles que
+ * estan asigandos a ese usuario.
+ * @param {type} param
+ */
+$(document).ready(function () {
+                    
+                       
+});
