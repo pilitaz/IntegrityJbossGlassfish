8be8f7c354906a3d862393df0b0dbe330697e64e
pilitaz <jennyp.gomezb@gmail.com>
2016-12-07 15:26:32 -0500

    Facturacion databound griddetalle

--- web/pedidos/js/pedido.js
+++ web/pedidos/js/pedido.js
@@ -5,3 +5,133 @@
  */
 
 
+$(document).ready(function() {   
+    
+    sessionStorage.setItem("cabeceraValida","false");    
+    iniDropDownList();
+    
+    iniAutocomplete();
+});
+
+function iniAutocomplete(){
+    
+}
+
+function iniDropDownList(){    
+        
+    //carga el combo de sucursales
+    $("#ipSucursal").kendoDropDownList({
+        optionLabel: "Seleccione la sucursal",
+        dataTextField: "suc__nom",
+        dataValueField: "suc__cod",
+        template:'<div class="divElementDropDownList">#: data.suc__nom #</div>',        
+        dataSource: {
+            transport: {
+                read: {
+                    url: ipServicios+"rest/Parameters/SIRSucursalagencia",
+                    contentType: "application/json; charset=utf-8",
+                    dataType: "json",
+                    type: "POST"
+                },
+                parameterMap: function (options, operation) {
+                    try {
+                        if (operation === 'read') {
+                            auth.dssic_suc.eetemp[0].piccia_nit = sessionStorage.getItem("companyNIT");                            
+                            auth["eesic_suc"] = [options];
+                            return JSON.stringify(auth);
+                        }	
+                    } catch (e) {
+                        alertDialogs(e.message);
+                    }
+                },
+            },
+            schema: {
+                type: "json",
+                data:function (e){
+                    if(e.dssic_suc.eeEstados[0].Estado==="OK"){
+                        return e.dssic_suc.eesic_suc;
+                    }else{
+                        alertDialogs(e.dssic_suc.eeEstados[0].Estado);
+                    }
+                },
+                model: {
+                    id: "suc__cod",
+                    fields: {
+                        suc__cod: {validation: {required: true}, type: 'string'},
+                        suc__nom: {validation: {required: true}, type: 'string'}
+                    }
+                }
+            },
+            error: function (xhr, error) {
+                alertDialogs("Error de conexion del servidor " +xhr.xhr.status+" "+ xhr.errorThrown);
+            }
+        }
+        
+    });
+    
+    
+    $("#ipCdePago").kendoDropDownList({
+        optionLabel: "Seleccione la forma de pago",
+        dataTextField: "pag__des",
+        dataValueField: "fac__pag"
+    });   
+    
+    // divisa
+    
+    
+    $("#ipDivisa").kendoDropDownList({
+        optionLabel: "Seleccione la moneda",
+        dataTextField: "mnd__des ",
+        dataValueField: "mnd__cla",
+        template:'<div class="divElementDropDownList">#: data.mnd__des #</div>',
+        change: onSelectDivisa,       
+        dataSource: {
+            transport: {
+                read: {
+                    url: ipServicios+"rest/Parameters/SIRsic_mnd",
+                    contentType: "application/json; charset=utf-8",
+                    dataType: "json",
+                    type: "POST"
+                },
+                parameterMap: function (options, operation) {
+                    try {
+                        if (operation === 'read') {
+                            authdssic_mnd["eesic_mnd"] = [options];                            
+                            return JSON.stringify(authdssic_mnd);
+                        }	
+                    } catch (e) {                        
+                        alertDialogs(e.message);
+                    }
+                }
+            },
+            schema: {
+                type: "json",
+                data:function (e){
+                    if(e.dssic_mnd.eeEstados[0].Estado==="OK"){
+                        return e.dssic_mnd.eesic_mnd;
+                    }else{
+                        alertDialogs("Problemas con el servicio: "+e.dssic_mnd.eeEstados[0].Estado);
+                    }
+                },
+                model: {
+                    id: "mnd__cla",
+                    fields: {
+                        mnd__cla: {validation: {required: true}, type: 'string'},
+                        mnd__des: {validation: {required: true}, type: 'string'}
+                    }
+                }
+            },
+            error: function (xhr, error) {
+                alertDialogs("Error de conexion del servidor " +xhr.xhr.status+" "+ xhr.errorThrown);
+            }
+        }
+        
+    });
+    
+    $("#ipVendedor").kendoDropDownList({
+        dataTextField: "ter__raz",        
+        dataValueField: "ven__cod", 
+        optionLabel: "Selecione un vendedor...",                
+        
+    });
+}
--- web/pedidos/js/pedidos.js
+++ web/pedidos/js/pedidos.js
@@ -1,8 +1,34 @@
 
 function crearPedido(){
-    var servicio = "pedido";
-    sessionStorage.setItem("servicio",servicio);
-    window.location.replace(( sessionStorage.getItem("url")+"pedidos/html/"+servicio+".html"));   
+  
+    var widthPopUp = $("body").width();
+    widthPopUp = widthPopUp * (80/100);
+    var heightPopUp = $("body").height();
+    heightPopUp = heightPopUp * (50/100);
+    
+    $("body").append("<div id='windowPedidoCabecera'></div>");
+    var myWindow = $("#windowPedidoCabecera");
+    var undo = $("#undo");
+    
+    function onCloseWindowItemFac() {
+        document.getElementById("windowPedidoCabecera").remove();            
+        undo.fadeIn();  
+    }
+    
+    myWindow.kendoWindow({
+        width: widthPopUp,
+        height: heightPopUp,
+        title: "Crear",
+        content: sessionStorage.getItem("url")+ "/pedidos/html/pedidoCabecera.html",
+        visible: false,
+        modal: true,
+        actions: [            
+            "Close"
+        ],
+        close: onCloseWindowItemFac
+    }).data("kendoWindow").center().open();    
 }
+
+
 
 
