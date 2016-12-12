7957201b31276031532a8aee73930f768350091f
alexgm5555 <alexgm5555@hotmail.com>
2016-12-12 11:00:48 -0500

    lista de pedidos sin Servicio

--- web/pedidos/js/pedidos.js
+++ web/pedidos/js/pedidos.js
@@ -102,36 +102,11 @@ function grid() {
     $("#gridPedidos .k-grid-header").css('display', 'none');
 }
 function crearPedido(){
-  
-    var widthPopUp = $("body").width();
-    widthPopUp = widthPopUp * (80/100);
-    var heightPopUp = $("body").height();
-    heightPopUp = heightPopUp * (50/100);
-    
-    $("body").append("<div id='windowPedidoCabecera'></div>");
-    var myWindow = $("#windowPedidoCabecera");
-    var undo = $("#undo");
-    
-    function onCloseWindowItemFac() {
-        document.getElementById("windowPedidoCabecera").remove();            
-        undo.fadeIn();  
-    }
-    
-    myWindow.kendoWindow({
-        width: widthPopUp,
-        height: heightPopUp,
-        title: "Crear",
-        content: sessionStorage.getItem("url")+ "/pedidos/html/pedidoCabecera.html",
-        visible: false,
-        modal: true,
-        actions: [            
-            "Close"
-        ],
-        close: onCloseWindowItemFac
-    }).data("kendoWindow").center().open();    
+  popUpPedidoCU();   
 }
 function ClickEditar(e){
     e = this.dataItem($(e.currentTarget).closest("tr"));
+    popUpPedidoCU();
     
 }
 
@@ -162,6 +137,34 @@ function clickEliminar(e){
         $('#gridPedidos').data('kendoGrid').dataSource.read();
         $('#gridPedidos').data('kendoGrid').refresh();
     }
+}
+function popUpPedidoCU (){
+    var widthPopUp = $("body").width();
+    widthPopUp = widthPopUp * (80/100);
+    var heightPopUp = $("body").height();
+    heightPopUp = heightPopUp * (50/100);
+    
+    $("body").append("<div id='windowPedidoCabecera'></div>");
+    var myWindow = $("#windowPedidoCabecera");
+    var undo = $("#undo");
+    
+    function onCloseWindowItemFac() {
+        document.getElementById("windowPedidoCabecera").remove();            
+        undo.fadeIn();  
+    }
+    
+    myWindow.kendoWindow({
+        width: widthPopUp,
+        height: heightPopUp,
+        title: "Crear",
+        content: sessionStorage.getItem("url")+ "/pedidos/html/pedidoCabecera.html",
+        visible: false,
+        modal: true,
+        actions: [            
+            "Close"
+        ],
+        close: onCloseWindowItemFac
+    }).data("kendoWindow").center().open();
 }
 
 
