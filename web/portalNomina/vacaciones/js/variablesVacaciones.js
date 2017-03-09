2226568dbf6c4f31c4bf8021e412876f5b585888
pilitaz <jennyp.gomezb@gmail.com>
2017-03-09 14:37:24 -0500

    Consulta vacaciones

---
+++ web/portalNomina/vacaciones/js/variablesVacaciones.js
@@ -0,0 +1,52 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function sirVacaciones() {
+    
+    var mapData = "ttconsutavacaiones";
+    var urlSir = ipServicios + baseNomina +"SIConsultavacaciones";
+    var json = {  
+        "dsSIConsultavacaciones":{  
+            "eeDatos":[  
+                {  
+                    "picusrcod":sessionStorage.getItem("usuario"),
+                    "picfiid":sessionStorage.getItem("picfiid"),
+                    "local_ip":sessionStorage.getItem("ipPrivada"),
+                    "remote_ip":sessionStorage.getItem("ipPublica")
+                }
+            ]
+        }
+    };
+    
+    this.setUrlSir = function (newname) {
+        if (newname) {
+            urlSir = newname;
+        }
+    };
+    this.getUrlSir = function () {
+        return urlSir;
+    };
+    
+    this.setjson = function (newname) {
+        if (newname) {
+            json = newname;
+        }
+    };
+    this.getjson = function () {
+        return json;
+    };
+    
+    this.setMapData = function (newname) {
+        if (newname) {
+            mapData = newname;
+        }
+    };
+    this.getMapData = function () {
+        return mapData;
+    };
+    
+};
