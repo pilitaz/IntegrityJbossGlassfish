d74feb56f79df26ed4a7a4ea9b8f09980446d672
pilitaz <jennyp.gomezb@gmail.com>
2017-03-13 16:44:43 -0500

    retencion en la fuente

---
+++ web/portalNomina/retencionEnLaFuente/js/variablesRetencionEnLaFuente.js
@@ -0,0 +1,55 @@
+/**
+ * Funcion para obtener la url y el json de entrada para los años
+ *  
+ *  ejemplo
+ *  var sir = new sirEjemplo();
+ *  var url = sir.getUrlSir();
+ *  var input = sir.getdataInputSir();
+ */
+
+function sirRetencionFuente() {
+    
+    var mapData = "ttconsultacomprobante";
+    var urlSir = ipServicios + baseNomina +"SIConsultaretefte";
+    var json = {  
+        "dsSIConsultaretefte":{  
+            "eeDatos":[  
+                {  
+//                    "picusrcod":sessionStorage.getItem("usuario"),
+                    "picusrcod": "jsalazar_800001541",
+                    "picfiid":sessionStorage.getItem("picfiid"),
+                    "local_ip":sessionStorage.getItem("ipPrivada"),
+                    "remote_ip":sessionStorage.getItem("ipPublica")
+                }
+            ]            
+        }
+    };
+    
+    this.setUrlSir = function (newname) {
+        if (newname) {
+            urlSir = newname;
+        }
+    };
+    this.getUrlSir = function () {
+        return urlSir;
+    };
+    
+    this.setjson = function (newname) {
+        if (newname) {
+            json = newname;
+        }
+    };
+    this.getjson = function () {
+        return json;
+    };
+    
+    this.setMapData = function (newname) {
+        if (newname) {
+            mapData = newname;
+        }
+    };
+    this.getMapData = function () {
+        return mapData;
+    };
+    
+};
