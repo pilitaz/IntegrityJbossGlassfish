5f7a9adcb58ea9a27d680ce1c1572875612f78ef
Desarrollo <Desarrollo@172.21.24.91>
2017-03-27 16:57:36 -0500

    cambios en procesos

--- web/procesos/formularioProcesos/js/Revisar_Solicitud.js
+++ web/procesos/formularioProcesos/js/Revisar_Solicitud.js
@@ -67,62 +67,69 @@ function aprovarVacaciones(){
     });  
   
 }
-function rechazarVacaciones(){
+function rechazarVacaciones(){debugger
     var obs = document.getElementById("ObservacionesJefe").value;
+  var obs = obs.trim();
+    if(obs===""){
+         parent.alertDialogs("Debe especificar observaciones de rechazo");
+    }
+    else
+    {
+           var datos= sessionStorage.getItem("Aprueba_Proceso");
+    var datos1 = JSON.parse(datos);
     
+    var consultar = new CudApruevaVacaciones();
+    var datajson = consultar.getjson();
+    var urlService = consultar.getUrlSir();
+    datajson.dsSolicitudVacaciones.SIRSolicitudVacaciones[0].picprocid=datos1.inst__name;
+    datajson.dsSolicitudVacaciones.SIRSolicitudVacaciones[0].pictaskname=datos1.task__name;
+    datajson.dsSolicitudVacaciones.SIRSolicitudVacaciones[0].pictaskname=datos1.task__name;
     
-//    var datos= sessionStorage.getItem("Aprueba_Proceso");
-//    var datos1 = JSON.parse(datos);
-//    
-//    var consultar = new CudApruevaVacaciones();
-//    var datajson = consultar.getjson();
-//    var urlService = consultar.getUrlSir();
-//    datajson.dsSolicitudVacaciones.SIRSolicitudVacaciones[0].picprocid=datos1.inst__name;
-//    datajson.dsSolicitudVacaciones.SIRSolicitudVacaciones[0].pictaskname=datos1.task__name;
-//    datajson.dsSolicitudVacaciones.SIRSolicitudVacaciones[0].pictaskname=datos1.task__name;
-//    
-//    datajson.dsSolicitudVacaciones.eeSolicitudVacaciones[0].Id_empleado=document.getElementById("usuario").innerHTML;
-//    datajson.dsSolicitudVacaciones.eeSolicitudVacaciones[0].email_empleado=document.getElementById("email").innerHTML;
-//    datajson.dsSolicitudVacaciones.eeSolicitudVacaciones[0].fecha_ult_vac= document.getElementById("fechaCorte").innerHTML;
-//    if (document.getElementById("pagoAnticipado").innerHTML==="Si"){
-//         datajson.dsSolicitudVacaciones.eeSolicitudVacaciones[0].Pago_anticipado=true;
-//    }else{
-//        datajson.dsSolicitudVacaciones.eeSolicitudVacaciones[0].Pago_anticipado=false;
-//    }
-//    
-//    datajson.dsSolicitudVacaciones.eeSolicitudVacaciones[0].dias_tiempo=document.getElementById("diasPedir").innerHTML;
-//    datajson.dsSolicitudVacaciones.eeSolicitudVacaciones[0].fecha_ini_vacaciones=  document.getElementById("corte").innerHTML;
-//    datajson.dsSolicitudVacaciones.eeSolicitudVacaciones[0].dias_dinero=document.getElementById("diasValor").innerHTML;
-//    datajson.dsSolicitudVacaciones.eeSolicitudVacaciones[0].jefe_inmediato=document.getElementById("jefe").innerHTML;
-//    datajson.dsSolicitudVacaciones.eeSolicitudVacaciones[0].dias_disponibles=document.getElementById("pendientes").innerHTML;
-//    datajson.dsSolicitudVacaciones.eeSolicitudVacaciones[0].dias_anticipados=document.getElementById("anticipacion").innerHTML;
-//    datajson.dsSolicitudVacaciones.eeSolicitudVacaciones[0].fecha_solictud= document.getElementById("fecha").innerHTML;
-//    datajson.dsSolicitudVacaciones.eeSolicitudVacaciones[0].observaciones_empleado = document.getElementById("observaciones").innerHTML;
-//    datajson.dsSolicitudVacaciones.eeSolicitudVacaciones[0].observaciones_jefe= document.getElementById("ObservacionesJefe").value; 
-//    datajson.dsSolicitudVacaciones.eeSolicitudVacaciones[0].estado_aprocbacion=false;        
-//    $.ajax({
-//        
-//        type: "POST",        
-//        async: false,
-//        data: JSON.stringify(datajson),
-//        url: urlService,
-//        dataType: "json",        
-//        contentType: "application/json;",
-//        success: function (resp) { debugger
-//            if((resp.dsSolicitudVacaciones.eeEstados[0].Estado)=="OK")
-//            {
-//               parent.terminarVacaciones(); 
-//            }
-//            else   
-//            {  
-//                
-//            } 
-//        } ,
-//        error: function (e) {
-//            parent.alertDialogs("Error al enviar la peticion" + e.status + " - " + e.statusText);
-//        }
-//        
-//    });  
+    datajson.dsSolicitudVacaciones.eeSolicitudVacaciones[0].Id_empleado=document.getElementById("usuario").innerHTML;
+    datajson.dsSolicitudVacaciones.eeSolicitudVacaciones[0].email_empleado=document.getElementById("email").innerHTML;
+    datajson.dsSolicitudVacaciones.eeSolicitudVacaciones[0].fecha_ult_vac= document.getElementById("fechaCorte").innerHTML;
+    if (document.getElementById("pagoAnticipado").innerHTML==="Si"){
+         datajson.dsSolicitudVacaciones.eeSolicitudVacaciones[0].Pago_anticipado=true;
+    }else{
+        datajson.dsSolicitudVacaciones.eeSolicitudVacaciones[0].Pago_anticipado=false;
+    }
+    
+    datajson.dsSolicitudVacaciones.eeSolicitudVacaciones[0].dias_tiempo=document.getElementById("diasPedir").innerHTML;
+    datajson.dsSolicitudVacaciones.eeSolicitudVacaciones[0].fecha_ini_vacaciones=  document.getElementById("corte").innerHTML;
+    datajson.dsSolicitudVacaciones.eeSolicitudVacaciones[0].dias_dinero=document.getElementById("diasValor").innerHTML;
+    datajson.dsSolicitudVacaciones.eeSolicitudVacaciones[0].jefe_inmediato=document.getElementById("jefe").innerHTML;
+    datajson.dsSolicitudVacaciones.eeSolicitudVacaciones[0].dias_disponibles=document.getElementById("pendientes").innerHTML;
+    datajson.dsSolicitudVacaciones.eeSolicitudVacaciones[0].dias_anticipados=document.getElementById("anticipacion").innerHTML;
+    datajson.dsSolicitudVacaciones.eeSolicitudVacaciones[0].fecha_solictud= document.getElementById("fecha").innerHTML;
+    datajson.dsSolicitudVacaciones.eeSolicitudVacaciones[0].observaciones_empleado = document.getElementById("observaciones").innerHTML;
+    datajson.dsSolicitudVacaciones.eeSolicitudVacaciones[0].observaciones_jefe= document.getElementById("ObservacionesJefe").value; 
+    datajson.dsSolicitudVacaciones.eeSolicitudVacaciones[0].estado_aprocbacion=false;        
+    $.ajax({
+        
+        type: "POST",        
+        async: false,
+        data: JSON.stringify(datajson),
+        url: urlService,
+        dataType: "json",        
+        contentType: "application/json;",
+        success: function (resp) { debugger
+            if((resp.dsSolicitudVacaciones.eeEstados[0].Estado)=="OK")
+            {
+               parent.terminarVacaciones(); 
+            }
+            else   
+            {  
+                
+            } 
+        } ,
+        error: function (e) {
+            parent.alertDialogs("Error al enviar la peticion" + e.status + " - " + e.statusText);
+        }
+        
+    });  
+    }
+    
+
 }
 
 function llenar(){debugger
