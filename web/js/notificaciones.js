2bd0d367a23c7b1c0d86585e8d36bf4b6f5a23ff
Desarrollo <Desarrollo@172.21.24.91>
2017-04-07 17:16:45 -0500

    Merge origin/master
    Conflicts:
    	web/html/index.html

--- web/js/notificaciones.js
+++ web/js/notificaciones.js
@@ -1,5 +1,5 @@
-             function notificaciones(){  debugger             
-                        
+function notificaciones(){  debugger             
+    
     var consultar = new sirNotificaciones();
     var datajson = consultar.getjson();
     var urlService = consultar.getUrlSir();
@@ -50,47 +50,129 @@
         }
     });
     
-        $("#gridNotificaciones").kendoGrid({
+    $("#gridNotificaciones").kendoGrid({
         dataSource: datasource,
-        scrollable: false,       
+        scrollable: false,  
+        selectable: true,
+        change: onChange,
         //navigatable: true,
         columns: [
             //                            { template: "<a class='k-grid-play'><span class='k-sprite pro_bullet1'></span></a>", width: "50px"} ,    
             {name: "imgNoti", text: " ",  template: "<a class='imgNoti'><img id='imgTransacciones' src='../images/transaccionesOff.png' /></a>",},
-                           
-           
+            
+            
             { field: "alt__msj", title: "",  hidden:false},
-           ],                            
-                            
+        ],                            
+        
         rowTemplate: kendo.template($("#rowTemplateCmp").html()),
         altRowTemplate: kendo.template($("#altRowTemplateCmp").html()),
         dataBound: function () {
             var results = datasource.data();
             changImgFunc(results);
         },
-       
+        
     });
- $("#gridNotificaciones .k-grid-header").css('display', 'none');
-                        
-                 
-                 function changImgFunc(results) {debugger
-                     
-                     for (var i = 0; i < results.length; i++) {
-                         //var nombre="spanplay"+results[i].alt__tip;
-                         switch (results[i].alt__tip) {
-                             case "procesos":
-                                 document.getElementById("imgNoti"+results[i].alt__id).attributes[4].nodeValue="../images/../images/procesosRO.png";
-//                                 document.getElementById("imgNoti"+results[i].alt__id).setAttribute("estado", "on");
-                                 break;
-                             case "reportes":
-                                  document.getElementById("imgNoti"+results[i].alt__id).attributes[4].nodeValue="../images/../images/reportesRO.png";
-                                 break;
-                             
-                         }                    
-                         
-                     }
-                     
-                 }
+    $("#gridNotificaciones .k-grid-header").css('display', 'none');
+    $(".k-grid").css("font-weight","bold");
+    $(".k-grid").css("font-size","14px");
+    
+    
+    function changImgFunc(results) {debugger
+        
+        for (var i = 0; i < results.length; i++) {
+            //var nombre="spanplay"+results[i].alt__tip;
+            switch (results[i].alt__tip) {
+                case "procesos":
+                    document.getElementById("imgNoti"+results[i].alt__id).attributes[4].nodeValue="../images/../images/procesosOn.png";
+                    //                                 document.getElementById("imgNoti"+results[i].alt__id).setAttribute("estado", "on");
+                    break;
+                case "reportes":
+                    document.getElementById("imgNoti"+results[i].alt__id).attributes[4].nodeValue="../images/../images/reportesOn.png";
+                    break;
+                case "portafolio":
+                    document.getElementById("imgNoti"+results[i].alt__id).attributes[4].nodeValue="../images/../images/portafolioOn.png";
+                    break;
+                case "transaciones":
+                    document.getElementById("imgNoti"+results[i].alt__id).attributes[4].nodeValue="../images/../images/transaccionesOn.png";
+                    break;
+            }              
+        }                    
+        
+        
+        for (var i = 0; i < results.length; i++) {
+            //var nombre="spanplay"+results[i].alt__tip;
+            switch (results[i].alt__est) {
+                case 3:
+                    $("#rowNoti"+results[i].alt__id).css("opacity", "0.4"); 
+                    break;
+                case 2:
+                    //$("#rowNoti"+results[i].alt__id).css("opacity", "0.4"); 
+                    break;
+                
+            }                    
+            
+        }
+        
+        
+        
+    }
 }             
 
-             
+function onChange(arg) {debugger
+    
+    var consultar = new cudNotificaciones();
+    var datajson = consultar.getjson();
+    var urlService = consultar.getUrlSir();
+    
+    
+    var x=arg.sender._data;       
+    var grid = $('#gridNotificaciones').data('kendoGrid');
+    var datos = grid.select($("#rowSelection_active_cell").closest("tr"))["0"].id;
+    var citrus = datos.slice(7,datos.lenght);
+    var i=0;
+    for (var i = 0; i < x.length; i++) {
+        
+        if( x[i].alt__id===parseInt(citrus) ){
+            var dataClick=x[i];
+            datajson.dsSICUDsic_alt.eesic_alt[0].alt__cad=dataClick.alt__cad;
+            datajson.dsSICUDsic_alt.eesic_alt[0].alt__dat=dataClick.alt__dat;
+            datajson.dsSICUDsic_alt.eesic_alt[0].alt__est=3;
+            datajson.dsSICUDsic_alt.eesic_alt[0].alt__id=dataClick.alt__id;
+            datajson.dsSICUDsic_alt.eesic_alt[0].alt__ini=dataClick.alt__ini;
+            datajson.dsSICUDsic_alt.eesic_alt[0].alt__msj=dataClick.alt__msj;
+            datajson.dsSICUDsic_alt.eesic_alt[0].func__name=dataClick.func__name;
+            datajson.dsSICUDsic_alt.eesic_alt[0].usr__cod=dataClick.usr__cod;
+            datajson.dsSICUDsic_alt.eesic_alt[0].usr__cod__g=dataClick.usr__cod__g;
+            datajson.dsSICUDsic_alt.eesic_alt[0].alt__tit=dataClick.alt__tit;
+            datajson.dsSICUDsic_alt.eesic_alt[0].alt__url=dataClick.alt__url;
+            datajson.dsSICUDsic_alt.eesic_alt[0].alt__tip=dataClick.alt__tip;
+            datajson.dsSICUDsic_alt.eesic_alt[0].alt__datehour=dataClick.alt__datehour;
+            $.ajax({
+                type: "PUT",
+                async: false, 
+                data: JSON.stringify(datajson),
+                url: urlService,
+                dataType : "json",  
+                contentType: "application/json;",
+                success: function (resp) {             
+                    if((resp.dsSICUDsic_alt.eeEstados["0"].Estado)=="OK")
+                    {
+                        
+                        $('#gridNotificaciones').data('kendoGrid').refresh();                                             
+                        $('#gridNotificaciones').data('kendoGrid').dataSource.read();
+                        $('#gridNotificaciones').data('kendoGrid').refresh(); 
+                    }
+                    else
+                    {
+                        
+                    }
+                },
+                error: function (e) {              
+                }
+            });
+            
+        }
+        
+        
+    }
+}      
